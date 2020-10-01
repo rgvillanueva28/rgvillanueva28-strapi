f37476441b5264fcd751587f1629bacdd2d59725
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-10-01 16:02:59 +0800

    🔼Update database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,6 @@
+const parse = require('pg-connection-string').parse;
+const config = parse(process.env.DATABASE_URL)
+
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
@@ -13,7 +16,6 @@ module.exports = ({ env }) => ({
       },
       options: {
         ssl: false,
-        useNullAsDefault: true,
       },
     },
   },
