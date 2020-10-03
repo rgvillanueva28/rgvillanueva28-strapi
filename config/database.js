bfbc9642cbb825cb81b1b285b4b1279c54da354f
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-10-03 12:06:22 +0800

    🖼Fixed cloudinary  settings.

--- config/database.js
+++ config/database.js
@@ -13,19 +13,10 @@ module.exports = ({ env }) => ({
         database: config.database,
         username: config.user,
         password: config.password,
-        ssl: true,
       },
       options: {
-        ssl: true,
+        ssl: false,
       },
-    },
-  },
-  upload: {
-    provider: "cloudinary",
-    providerOptions: {
-      cloud_name: env("CLOUDINARY_NAME"),
-      api_key: env("CLOUDINARY_KEY"),
-      api_secret: env("CLOUDINARY_SECRET"),
     },
   },
 });
