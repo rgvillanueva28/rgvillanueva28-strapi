f6fc416abb0c130dc606fc4f876f32ab37cd2bfc
Rane Gillian Villanueva <rgvillanueva28@gmail.com>
2020-10-03 11:42:40 +0800

    🖼Added cloudinary image storage

--- config/database.js
+++ config/database.js
@@ -1,22 +1,31 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL)
+const parse = require("pg-connection-string").parse;
+const config = parse(process.env.DATABASE_URL);
 
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'bookshelf',
+      connector: "bookshelf",
       settings: {
-        client: 'postgres',
+        client: "postgres",
         host: config.host,
         port: config.port,
         database: config.database,
         username: config.user,
         password: config.password,
+        ssl: true,
       },
       options: {
-        ssl: false,
+        ssl: true,
       },
+    },
+  },
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
     },
   },
 });
